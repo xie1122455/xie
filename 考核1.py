e8738ce7916deebe2cdda97bd98ff4538250dea2
xie <email@2762568518.com>
2025-10-26 19:44:16 +0800

    111

--- 考核1.py
+++ 考核1.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
 import os
+import scipy.sparse as sp
 
 class CoraDataset(Dataset):
     def __init__(self,data_root="cora/",transfrom=None):
@@ -13,11 +14,10 @@ class CoraDataset(Dataset):
         self.labels = np.unique(content[:,-1], return_inverse=True)[1].astype(np.int64)
         cites = np.genfromtxt(os.path.join(data_root, 'Cora.cites'), dtype=int)
         edges = np.searchsorted(content[:,0].astype(int), cites)
+        self.adj = sp.coo_matrix((np.ones(len(np.unique(np.vstack([edges, edges[:,::-1]]), axis=0))), 
+                                 tuple(np.unique(np.vstack([edges, edges[:,::-1]]), axis=0).T)), 
+                                shape=(len(self.labels),)*2)
     def __getitem__(self, idx): 
         return torch.tensor(self.features[idx]), torch.tensor(self.labels[idx]) 
     def __len__(self):
         return len(self.labels)
-    
-    
-
-    
